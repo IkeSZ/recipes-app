08d23bb9290fc8b867e7969d03d8f123aa7460d1
aizackSF <70985216+aizackSF@users.noreply.github.com>
2021-03-14 16:03:32 -0300

    Added CategoriesMeals screen and added navigation between screens

--- lib/components/categoryItem.dart
+++ lib/components/categoryItem.dart
@@ -3,28 +3,45 @@ import 'package:flutter/material.dart';
 // Models
 import '../models/category.dart';
 
+// Screens
+import '../screens/categoriesMeals.dart';
+
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoriesMealsScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext ctx) {
-    return Container(
-      padding: EdgeInsets.all(15),
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
 
-      child: Text(category.title, style: Theme.of(ctx).textTheme.headline6),
+      onTap: () => _selectCategory(ctx),
 
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        
-        gradient: LinearGradient(colors: [
-          category.color.withOpacity(0.5),
-          category.color
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight
-       )
+      child: Container(
+        padding: EdgeInsets.all(15),
+
+        child: Text(category.title, style: Theme.of(ctx).textTheme.headline6),
+
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+
+            gradient: LinearGradient(colors: [
+              category.color.withOpacity(0.5),
+              category.color
+            ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight
+            )
+        ),
       ),
     );
   }
---
+++ lib/screens/categoriesMeals.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Receitas', style: TextStyle(color: Colors.white)),
+      ),
+
+      body: Center(
+        child: Text('oficialmente desinstalando o aplicativo whatsapp'),
+      ),
+    );
+  }
+}
